4f5134161ea59f9424fc9cf299c9fd6d1625d4d2
ChandlerHaut <hauter357@gmail.com>
2024-01-28 19:28:38 -0700

    Week 3

--- Assignments/Assignment_3/Assignment_3.R
+++ Assignments/Assignment_3/Assignment_3.R
@@ -153,30 +153,38 @@ dbinom(50,100,.5) # probability of getting exactly 50 heads out of 100 coin flip
 
 # YOUR REMAINING HOMEWORK ASSIGNMENT (Fill in with code) ####
 
+#Assignment 3 (C. Haut)
 # 1.  Get a subset of the "iris" data frame where it's just even-numbered rows
 
 seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
+iris <- iris
 
-
+iris[seq(2,150,2),]
 
 # 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
+iris_chr <- iris
 
+for (col in names(iris_chr)) {
+  iris_chr[,col] <- as.character(iris_chr[,col])
+}
 
-
-# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
+# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width 
+Sepal.Area <- iris$Sepal.Length * iris$Sepal.Width
 
 
 
 # 4.  Add Sepal.Area to the iris data frame as a new column
-
+iris$Sepal.Area = iris$Sepal.Length * iris$Sepal.Width
 
 
 # 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
-      # (name it big_area_iris)
-
+# (name it big_area_iris)
+library(tidyverse)
+big_area_iris <- 
+  iris %>% 
+  filter(Sepal.Area > 20)
 
 
 # 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
-      # to canvas
-      # I should be able to run your R script and get all the right objects generated
-
+# to canvas
+# I should be able to run your R script and get all the right objects generated
